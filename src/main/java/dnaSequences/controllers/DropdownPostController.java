91847183bdd582e5a06725bc5593620cf63577e3
Joe Morris <joe.morris.dev@gmail.com>
2017-01-16 20:04:29 -0600

    outline

---
+++ src/main/java/dnaSequences/controllers/DropdownPostController.java
@@ -0,0 +1,18 @@
+package dnaSequences.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by joe on 1/16/17.
+ */
+public class DropdownPostController {
+
+    @RequestMapping(path = "/dropdown", method = RequestMethod.POST)
+    public String selectDocument(HttpSession session, String document){
+
+        return "redirect:/displayResults";
+    }
+}
