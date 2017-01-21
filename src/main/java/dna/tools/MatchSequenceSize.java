2c11923526be82fc1cffdc108e671709c5c38b07
Joe Morris <joe.morris.dev@gmail.com>
2017-01-20 19:54:49 -0600

    just created

---
+++ src/main/java/dna/tools/MatchSequenceSize.java
@@ -0,0 +1,15 @@
+package dna.tools;
+
+import dna.virus.Virus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by joe on 1/20/17.
+ */
+public class MatchSequenceSize {
+
+    public static void matchSequenceSize(String convertedText, ArrayList<Virus> viruses){
+        
+    }
+}
