53eb268d029c0209f737f599273c1a5d5c1cd639
Joe Morris <joe.morris.dev@gmail.com>
2017-01-21 01:53:56 -0600

    testing different methods

--- src/main/java/dna/controllers/UploadPostController.java
+++ src/main/java/dna/controllers/UploadPostController.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import dna.converters.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by joe on 1/15/17.
@@ -22,10 +24,12 @@ public class UploadPostController {
     //TODO: security concerns?
     //TODO: fix issue with reading docs in and getting text
     @RequestMapping(path="/upload", method = RequestMethod.POST)
-    public String uploadDocument(HttpSession session, MultipartFile document){
+    public String uploadDocument(HttpServletRequest request, HttpSession session, MultipartFile document)throws IOException{
 
+        //ways that didn't work
         //CheckFileType.checkFileType(document);
-        String convertedDocument = document.toString();
+        //String convertedDocument = document.toString();
+        //String convertedDocument = request.getInputStream().toString();
 
         session.setAttribute("rawText", convertedDocument);
 
