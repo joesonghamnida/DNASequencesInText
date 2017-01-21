3c16773dba21dd6460bebabbddad9080c5c13e43
Joe Morris <joe.morris.dev@gmail.com>
2017-01-20 20:42:28 -0600

    tested sequence sizing

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,5 +1,11 @@
+import dna.virus.Virus;
 import org.junit.Assert;
 import org.junit.Test;
+
+import dna.converters.*;
+import dna.tools.MatchSequenceSize;
+
+import java.util.ArrayList;
 
 public class Tests {
 
@@ -11,4 +17,24 @@ public class Tests {
         Assert.assertFalse("AGFT".equals(ConvertTextToSequence.convertTextToSequence("agct")));
     }
 
+    @Test
+    public void matchSequenceSizeConvertedStringShorter(){
+        String convertedString = "AAA";
+        Virus virus = new Virus("test", "AAAC");
+        ArrayList<Virus> viruses = new ArrayList<>();
+        viruses.add(virus);
+        MatchSequenceSize.matchSequenceSize(convertedString, viruses);
+        Assert.assertTrue(convertedString.length() == virus.getSequence().length());
+    }
+
+    @Test
+    public void matchSequenceSizeConvertedStringLonger(){
+        String convertedString = "AAACG";
+        Virus virus = new Virus("test", "AAAC");
+        ArrayList<Virus> viruses = new ArrayList<>();
+        viruses.add(virus);
+        MatchSequenceSize.matchSequenceSize(convertedString, viruses);
+        Assert.assertTrue(convertedString.length() == virus.getSequence().length());
+        System.out.println(virus.getSequence());
+    }
 }
