f021a81d84da6e36f7ab59f3f3382505905e85d9
Joe Morris <joe.morris.dev@gmail.com>
2017-01-20 23:07:28 -0600

    document bug

--- src/main/java/dna/controllers/UploadPostController.java
+++ src/main/java/dna/controllers/UploadPostController.java
@@ -1,5 +1,6 @@
 package dna.controllers;
 
+import dna.tools.CheckFileType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpSession;
 
 import dna.converters.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 /**
  * Created by joe on 1/15/17.
@@ -15,9 +19,15 @@ import dna.converters.*;
 @Controller
 public class UploadPostController {
 
+    //TODO: security concerns?
+    //TODO: fix issue with reading docs in and getting text
     @RequestMapping(path="/upload", method = RequestMethod.POST)
-    public String uploadDocument(HttpSession session, String document){
-        session.setAttribute("rawText",document);
+    public String uploadDocument(HttpSession session, MultipartFile document){
+
+        //CheckFileType.checkFileType(document);
+        String convertedDocument = document.toString();
+
+        session.setAttribute("rawText", convertedDocument);
 
         return "redirect:/displayResults";
     }
