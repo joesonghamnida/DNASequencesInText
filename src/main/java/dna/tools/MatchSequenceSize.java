3d5361e1cd05e0cba6a18751264e2edc7235405c
Joe Morris <joe.morris.dev@gmail.com>
2017-01-20 20:30:59 -0600

    built out

--- src/main/java/dna/tools/MatchSequenceSize.java
+++ src/main/java/dna/tools/MatchSequenceSize.java
@@ -10,6 +10,22 @@ import java.util.ArrayList;
 public class MatchSequenceSize {
 
     public static void matchSequenceSize(String convertedText, ArrayList<Virus> viruses){
-        
+
+        for(Virus virus : viruses){
+            if(convertedText.length() < virus.getSequence().length()){
+                String subset = virus.getSequence().substring(0, convertedText.length() - 1);
+                virus.setSequence(subset);
+            }
+        }
+
+        for(Virus virus : viruses){
+            if(convertedText.length() > virus.getSequence().length()){
+                String holding = "";
+                for(int i = 0; i < (convertedText.length() - virus.getSequence().length());i++){
+                    holding = holding + virus.getSequence().toString().charAt(i);
+                }
+                virus.setSequence(virus.getSequence() + holding);
+            }
+        }
     }
 }
