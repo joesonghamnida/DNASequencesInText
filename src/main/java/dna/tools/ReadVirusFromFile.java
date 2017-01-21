779545797ccd58938fec2da8c744d9a79db649bd
Joe Morris <joe.morris.dev@gmail.com>
2017-01-20 18:36:05 -0600

    basic outline

---
+++ src/main/java/dna/tools/ReadVirusFromFile.java
@@ -0,0 +1,16 @@
+package dna.tools;
+
+import dna.virus.Virus;
+
+/**
+ * Created by joe on 1/20/17.
+ */
+public class ReadVirusFromFile {
+
+    public static Virus readVirusFromFile(){
+
+        Virus virus = new Virus();
+
+        return virus;
+    }
+}
