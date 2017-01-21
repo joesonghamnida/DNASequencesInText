6272c2edb819a8cc306119f3f4a7d9ab61eb122d
Joe Morris <joe.morris.dev@gmail.com>
2017-01-20 18:35:35 -0600

    added information for research

--- src/main/java/dna/tools/CheckFileType.java
+++ src/main/java/dna/tools/CheckFileType.java
@@ -1,7 +1,24 @@
 package dna.tools;
 
+import java.io.File;
+import java.nio.file.Files;
+
 /**
  * Created by joe on 1/20/17.
  */
 public class CheckFileType {
+
+    public static Boolean checkFileType(File document){
+        Boolean fileTypePermitted = false;
+
+        //https://www.ietf.org/rfc/rfc2045.txt
+
+        //Files.probeContentType()
+        //Files.isExecutable()
+
+        //maybe break this into two functions, one to check for illegal file types (jpeg, pdf, etc)
+        //and one to check for .exes?
+
+        return fileTypePermitted;
+    }
 }
