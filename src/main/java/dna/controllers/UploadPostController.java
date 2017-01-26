b99ccb10099d442fd11cd94187bb493a3cc8535f
Joe Morris <joe.morris.dev@gmail.com>
2017-01-26 16:18:12 -0600

    fixed issue with upload

--- src/main/java/dna/controllers/UploadPostController.java
+++ src/main/java/dna/controllers/UploadPostController.java
@@ -10,8 +10,11 @@ import javax.servlet.http.HttpSession;
 
 import dna.converters.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -24,14 +27,24 @@ public class UploadPostController {
     //TODO: security concerns?
     //TODO: fix issue with reading docs in and getting text
     @RequestMapping(path="/upload", method = RequestMethod.POST)
-    public String uploadDocument(HttpServletRequest request, HttpSession session, MultipartFile document)throws IOException{
+    public String uploadDocument(HttpSession session, MultipartFile document)throws IOException{
 
         //ways that didn't work
         //CheckFileType.checkFileType(document);
-        //String convertedDocument = document.toString();
+        //int convertedDocument = document.getInputStream().read();
         //String convertedDocument = request.getInputStream().toString();
+        //System.out.println(convertedDocument);
+        //possibly use temp file on tomcat server?
+        //MultipartResolver resolver
 
-        session.setAttribute("rawText", convertedDocument);
+        File dir = new File("./");
+        dir.mkdirs();
+
+        File f = File.createTempFile("file", document.getOriginalFilename(), dir);
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(document.getBytes());
+
+        session.setAttribute("rawText", document.getOriginalFilename());
 
         return "redirect:/displayResults";
     }
